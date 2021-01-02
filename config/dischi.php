1fcdbd303ec5a243f8883be1ffc8fd14a6c95cce
Marco <marco.chianese93@gmail.com>
2021-01-02 11:42:42 +0100

    stampa dischi  anno < 1995 (v1)

---
+++ config/dischi.php
@@ -0,0 +1,86 @@
+<?php
+
+return [
+    [
+        "artista" => "The Queen",
+        "genere" => "rock",
+        "titolo" => "A Kind of Magic",
+        "anno" => "1986",
+        "numero_tracce" => "9",
+        "nazione_artista" => "inghilterra"
+    ],
+    [
+        "artista" => "Spice Girls",
+        "genere" => "pop",
+        "titolo" => "Spice",
+        "anno" => "1996",
+        "numero_tracce" => "10",
+        "nazione_artista" => "inghilterra"
+    ],
+    [
+        "artista" => "Cesare Cremonini",
+        "genere" => "pop",
+        "titolo" => "Il primo bacio sulla Luna",
+        "anno" => "2008",
+        "numero_tracce" => "12",
+        "nazione_artista" => "italia"
+    ],
+    [
+        "artista" => "The Clash",
+        "genere" => "rock",
+        "titolo" => "London Calling",
+        "anno" => "1979",
+        "numero_tracce" => "19",
+        "nazione_artista" => "inghilterra"
+    ],
+    [
+        "artista" => "The Queen",
+        "genere" => "rock",
+        "titolo" => "A Day at the Races",
+        "anno" => "1976",
+        "numero_tracce" => "10",
+        "nazione_artista" => "inghilterra"
+    ],
+    [
+        "artista" => "The Clash",
+        "genere" => "rock",
+        "titolo" => "Combat Rock",
+        "anno" => "1982",
+        "numero_tracce" => "12",
+        "nazione_artista" => "inghilterra"
+    ],
+    [
+        "artista" => "Samuele Bersani",
+        "genere" => "pop",
+        "titolo" => "Nuvola numero nove",
+        "anno" => "2013",
+        "numero_tracce" => "10",
+        "nazione_artista" => "italia"
+    ],
+    [
+        "artista" => "Michael Jackson",
+        "genere" => "pop",
+        "titolo" => "Thriller",
+        "anno" => "1982",
+        "numero_tracce" => "9",
+        "nazione_artista" => "stati uniti"
+    ],
+    [
+        "artista" => "Gigi D'Agostino",
+        "genere" => "dance",
+        "titolo" => "Disco Tanz",
+        "anno" => "2005",
+        "numero_tracce" => "30",
+        "nazione_artista" => "italia"
+    ],
+    [
+        "artista" => "Samuele Bersani",
+        "genere" => "pop",
+        "titolo" => "Caramella smog",
+        "anno" => "2003",
+        "numero_tracce" => "11",
+        "nazione_artista" => "italia"
+    ]
+];
+
+?>
